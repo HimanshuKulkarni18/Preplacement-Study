39932a11e4e1d65ed221e0026d7ce0c03816b024
Himanshu Kulkarni <153819237+HimanshuKulkarni18@users.noreply.github.com>
2026-07-25 22:59:22 +0530

    Add files via upload

---
+++ 25-07-2026.py
@@ -0,0 +1,34 @@
+'''This is a comment
+written in 
+more than just one line'''
+
+for x in "banana":
+    print(x)
+    
+a = "Hello, World!"
+print(a.upper())
+
+txt = "The best things in life are free!"
+if "free" in txt:
+    print("Yes,'free' is present")
+
+b = "hello, world!"
+print(b[-5:-2])
+
+c = "Hello"
+d = "World"
+e = c + " " + d
+print(e)
+
+age = 23
+txt = f"my name s Himanshu, and i am {age} years old."
+print(txt)
+
+price = 49
+pxt = f"The price is {price:.2f} dollars"
+txt = f"The price is {25 * 4:.2f} dollars"
+print(pxt)
+print(txt)
+
+Harry = "We are the so-called \"Vikings\" from the north."
+print(Harry)
