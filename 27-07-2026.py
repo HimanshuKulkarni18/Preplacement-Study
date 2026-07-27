51b6a0b833b31f0ac4b1e3a422405aeb2dea3d5f
Himanshu Kulkarni <153819237+HimanshuKulkarni18@users.noreply.github.com>
2026-07-27 22:14:56 +0530

    Add files via upload

---
+++ 27-07-2026.py
@@ -0,0 +1,107 @@
+class MyClass:
+    def __len__(self):
+        return 0
+    
+myjob = MyClass()
+print(bool(myjob))
+
+def myFunction():
+    return True 
+print(myFunction())
+
+def my_function():
+    return True
+if my_function():
+    print("True")
+else:
+    print("False")
+    
+sum1 = 120 + 80 
+sum2 = sum1 + 100
+sum3 = sum2 + 50
+print(sum3)
+
+a = 10 % 3
+print(a)
+
+b = 10 // 3
+print(b)
+
+c = 10 ** 3
+print(c)
+
+d = 10 / 3
+print(d)
+
+e = 10 * 3
+print(e)
+
+f = 10 - 3
+print(f)
+
+g = 10 + 3
+print(g)
+
+h = 10 > 3
+print(h)
+
+i = 10 < 3
+print(i)
+
+j = 10 == 3
+print(j)
+
+k = 10 != 3
+print(k)
+
+l = 10 >= 3
+print(l)
+
+m = 10 <= 3
+print(m)
+
+n = 10 and 3
+print(n)
+
+o = 10 or 3
+print(o)
+
+p = not 10
+print(p)
+
+q = 10 is 3
+print(q)
+
+r = 10 is not 3
+print(r)
+
+s = 10 in [1, 2, 3, 4, 5]
+print(s)
+
+t = 10 not in [1, 2, 3, 4, 5]
+print(t)
+
+u = 10 if 3 > 5 else 3
+print(u)
+
+v = 10 if 3 < 5 else 3
+print(v)
+
+w = 10 if 3 == 5 else 3
+print(w)
+
+x = 10 if 3 != 5 else 3
+print(x)
+
+y = 10 if 3 >= 5 else 3
+print(y)
+
+z = 10 if 3 <= 5 else 3
+print(z)
+
+
+a += 5
+print(x)
+
+b = 2 ** 4
+print(b)
